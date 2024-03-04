7936b933a31b5a97d05c2f42d6c7739a5d19e80c
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2024-03-04 15:26:16 +0700

    post new todo via api and redirect to home screen

--- src/app/add-todo/page.tsx
+++ src/app/add-todo/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { z } from 'zod';
+import { useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import TodoService from '@/services/todos-services';
 
 const formSchema = z.object({
   name: z.string().min(3, { message: 'Min 3 chars' }),
@@ -12,6 +16,8 @@ const formSchema = z.object({
 type FormValuesType = z.infer<typeof formSchema>;
 
 export default function Todos() {
+  const { push } = useRouter();
+  const [error, setError] = useState(false);
   const formHandlers = useForm<FormValuesType>({
     mode: 'all',
     resolver: zodResolver(formSchema),
@@ -22,46 +28,67 @@ export default function Todos() {
     formState: { errors },
   } = formHandlers;
 
-  const onSubmit = (data: FormValuesType) => {
-    console.log('FORM_VALUES', { data });
+  const resetError = () => setTimeout(() => setError(false), 2000);
+
+  const onSubmit = async ({ name }: FormValuesType) => {
+    try {
+      setError(false);
+      await TodoService.addTodo(name);
+      push('/');
+    } catch (error) {
+      console.log('ERROR', { error });
+      setError(true);
+      resetError();
+    }
   };
 
   return (
-    <main className="container flex-col flex p-4 gap-4">
-      <h1 className="self-center">Add New Todo</h1>
-      <FormProvider {...formHandlers}>
-        <form
-          className="flex flex-col gap-4"
-          onSubmit={formHandlers.handleSubmit(onSubmit)}
-        >
-          <label className="form-control w-full">
-            <div className="label">
-              <span className="label-text">New Todo</span>
+    <>
+      <main className="container flex-col flex p-4 gap-4">
+        <h1 className="self-center">Add New Todo</h1>
+        <FormProvider {...formHandlers}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={formHandlers.handleSubmit(onSubmit)}
+          >
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">New Todo</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+                {...register('name')}
+              />
+              <div className="label">
+                {errors['name'] && (
+                  <span className="label-text-alt text-red-600">
+                    {`${errors['name']?.message}`}
+                  </span>
+                )}
+              </div>
+            </label>
+            <div className="flex justify-end gap-2">
+              <Link href="/">
+                <button className="btn btn-error btn-sm">Cancel</button>
+              </Link>
+              <button type="submit" className="btn btn-primary btn-sm self-end">
+                Submit
+              </button>
             </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered w-full"
-              {...register('name')}
-            />
-            <div className="label">
-              {errors['name'] && (
-                <span className="label-text-alt text-red-600">
-                  {`${errors['name']?.message}`}
-                </span>
-              )}
-            </div>
-          </label>
-          <div className="flex justify-end gap-2">
-            <Link href="/">
-              <button className="btn btn-error btn-sm">Cancel</button>
-            </Link>
-            <button type="submit" className="btn btn-primary btn-sm self-end">
-              Submit
-            </button>
+          </form>
+        </FormProvider>
+      </main>
+
+      {/* ERROR ALERT */}
+      <div className="toast toast-top toast-center">
+        {error && (
+          <div className="alert alert-error">
+            <span>Something went wrong. Please try again later.</span>
           </div>
-        </form>
-      </FormProvider>
-    </main>
+        )}
+      </div>
+    </>
   );
 }
---
+++ src/services/todos-services.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+import { Todo } from '@/models/todo-models';
+
+const getTodos = async (): Promise<Todo[]> => {
+  return axios.get('api/todos');
+};
+
+const addTodo = async (name: string): Promise<Todo> => {
+  return axios.post('api/todos', { name });
+};
+
+const updateTodo = async (todo: Todo): Promise<Todo> => {
+  return axios.patch('api/todos', todo);
+};
+
+const todosService = {
+  getTodos,
+  addTodo,
+  updateTodo,
+};
+
+export default todosService;
